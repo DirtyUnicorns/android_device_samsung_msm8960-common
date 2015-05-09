ed869213c5b0646d49d2b55346ee1ad0bb9c702d
Michael Bestas <mikeioannina@gmail.com>
2015-05-10 02:02:36 +0300

    msm8960: optimize ART
    Dexopt builds
    Turn off ART watchdog
    
    Conflicts:
    	msm8960.mk
    
    Change-Id: I95f788ae6a7aca61dd839517300eea05833c0f38

--- msm8960.mk
+++ msm8960.mk
@@ -164,22 +164,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
 
-# NFC
-PRODUCT_PACKAGES += \
-    libnfc \
-    libnfc_jni \
-    Nfc \
-    Tag \
-    com.android.nfc_extras
-
-ifeq ($(TARGET_BUILD_VARIANT),user)
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access.xml
-else
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access_debug.xml
-endif
-PRODUCT_COPY_FILES += \
-    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
-
 # OMX
 PRODUCT_PACKAGES += \
     libdashplayer \
@@ -211,6 +195,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory \
     persist.sys.isUsbOtgEnabled=true
+
+# ART
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-flags=--no-watch-dog
 
 # Wifi
 PRODUCT_PACKAGES += \
